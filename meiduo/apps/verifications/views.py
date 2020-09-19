22aca2cff8fff4b6b9e1f26fdfd914c6af39e5e9
xwp <xwp_fullstack@163.com>
2020-09-19 21:53:00 +0800

    图片验证码,短信验证码

--- meiduo/apps/verifications/views.py
+++ meiduo/apps/verifications/views.py
@@ -3,7 +3,48 @@ from django.views import View
 from verifications.libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
 from django import http
+from . import constants
+from meiduo.utils.response_code import RETCODE
+import random
+from verifications.libs.yuntongxun.ccp_sms import CPP
 # Create your views here.
+
+class SMSCodeView(View):
+    """短信验证码"""
+    def get(self,request,mobile):
+        """
+        :param request:
+        :param mobile:手机号
+        :return: json
+        """
+        # 接收参数
+        img_code_client = request.GET.get('image_code')
+        # print(img_code_client)
+        uuid = request.GET.get('uuid')
+        # 校验参数
+        if not all([img_code_client,uuid]):
+            return http.HttpResponseForbidden('缺少必要参数')
+
+        # 提取图形验证码
+        redis_conn = get_redis_connection('verify_code')
+        img_code_server = redis_conn.get("img_%s" % uuid)   # 这是一个bytes类型
+        if img_code_server is None:
+            return http.JsonResponse({'code':RETCODE.IMAGECODEERR,'errmsg':'图形验证码已经失效'})
+        # 删除图形验证码
+        redis_conn.delete("img_%s" % uuid)
+        # 对比图形验证码，将bytes类型转为字符串再比较
+        img_code_server = img_code_server.decode()
+        if img_code_server.lower() != img_code_client.lower():
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码有误'})
+
+        # 生产短信验证码 %06d代表，如果不够6位，前面补0
+        sms_code = '%06d' % random.randint(0,999999)
+        # 保存短信验证码
+        redis_conn.setex("sms_%s" % mobile,constants.SMS_CODE_REDIS_EXPIRES,sms_code)
+        # 发送短信验证码
+        CPP().send_message_sms(mobile,[sms_code,constants.SMS_CODE_REDIS_EXPIRES // 60],constants.SEND_SMS_TEMPLATE_ID)
+        return http.JsonResponse({'code':RETCODE.OK,'errmsg':'发送短信成功'})
+
 
 class ImageCodeView(View):
     """图形验证码"""
@@ -20,6 +61,6 @@ class ImageCodeView(View):
         # 保存图片验证码
         redis_conn = get_redis_connection('verify_code')    # 找到dev中配置的redis库
         # 设置键为uuid，值为text，超时时间为300秒
-        redis_conn.setex("img_%s" % uuid,300,text)  # setex可以设置生存时间
+        redis_conn.setex("img_%s" % uuid,constants.IMAGE_CODE_REDIS_EXPIRES,text)  # setex可以设置生存时间
         # 响应图片验证码
         return http.HttpResponse(image,content_type='image/jpg')
--- meiduo/static/js/register.js
+++ meiduo/static/js/register.js
@@ -5,23 +5,28 @@
 var vm = new Vue({
     el: "#app",
     //修改Vue读取变量的方法
-    delimiters:['[[',']]'],
+    delimiters: ['[[', ']]'],
     data: {
         username: '',
         password: '',
         password2: '',
         mobile: '',
-        image_code_url:'',
-        uuid:'',
+        image_code_url: '',
+        uuid: '',
+        image_code: '',
+        sms_code_tip: '获取短信验证码',
+        send_flg : false,   //是否已经发送短信验证码
         //v-show
         error_name: false,
         error_password: false,
         error_password2: false,
         error_mobile: false,
         error_allow: false,
+        error_image_code: false,
         //error_message
         error_name_message: '',
-        error_mobile_message: ''
+        error_mobile_message: '',
+        error_image_code_message: ''
     },
     //页面一加载完，vue自动调用mounted函数
     mounted(){
@@ -32,7 +37,7 @@ var vm = new Vue({
         // 生成图形验证码的方法：封装思想，代码复用
         generate_image_code(){
             this.uuid = generateUUID();
-            this.image_code_url = '/image_codes/'+this.uuid + '/';
+            this.image_code_url = '/image_codes/' + this.uuid + '/';
         },
         //校验用户名
         check_username(){
@@ -45,22 +50,22 @@ var vm = new Vue({
             }
 
             //判断用户名是否重复
-            if(this.error_name == false){
+            if (this.error_name == false) {
                 //只有当用户输入的用户名满足条件时，才会继续
                 //两个参数，一个请求地址，一个请求头
                 let url = '/usernames/' + this.username + '/count/';
-                axios.get(url,{
-                    responseType:'json',
+                axios.get(url, {
+                    responseType: 'json',
                 }).then(response => {
-                   if(response.data.count == 1){
-                       //用户名已存在
-                       this.error_name_message = '用户名已存在';
-                       this.error_name = true;
-                   }
-                   else{
-                       //用户名不存在
-                       this.error_name = false;
-                   }
+                    if (response.data.count == 1) {
+                        //用户名已存在
+                        this.error_name_message = '用户名已存在';
+                        this.error_name = true;
+                    }
+                    else {
+                        //用户名不存在
+                        this.error_name = false;
+                    }
                 }).catch(error => {
                     console.log(error.response);
                 });
@@ -95,21 +100,30 @@ var vm = new Vue({
             }
 
             //校验手机号是否重复
-            if(this.error_mobile == false){
+            if (this.error_mobile == false) {
                 let url = '/mobile/' + this.mobile + '/count/'
-                axios.get(url,{
-                    responseType:'json',
+                axios.get(url, {
+                    responseType: 'json',
                 }).then(response => {
-                    if(response.data.count == 1){
+                    if (response.data.count == 1) {
                         this.error_mobile_message = '此电话号码已存在';
                         this.error_mobile = true;
                     }
-                    else{
+                    else {
                         this.error_mobile = false;
                     }
                 }).catch(error => {
                     console.log(error.response);
                 })
+            }
+        },
+        check_image_code(){
+            if (this.image_code.length < 4) {
+                this.error_image_code_message = "请输入四位图形验证码";
+                this.error_image_code = true;
+            }
+            else {
+                this.error_image_code = false;
             }
         },
         //校验协议勾选
@@ -119,6 +133,56 @@ var vm = new Vue({
             } else {
                 this.error_allow = false;
             }
+        },
+        //发送短信验证码
+        send_sms_code(){
+            //避免用户频繁点击获取验证码的标签
+            if(this.send_flg == true){
+                return
+            }
+            this.send_flg = false;
+            //校验mobile和image_code
+            this.check_mobile();
+            this.check_image_code();
+            if(this.error_mobile == true || this.error_image_code == true){
+                return;
+            }
+
+            let url = "/sms_codes/" + this.mobile + "/?image_code=" + this.image_code + "&uuid=" + this.uuid;
+            axios.get(url, {
+                responseType: 'json'
+            })
+                .then(response => {
+                    if (response.data.code == '0') {
+                        //展示倒计时60s,创建一个计时器
+                        //setInterval('回调函数','时间间隔');
+                        let num = 60;
+                        let no = setInterval(() =>{
+                            if(num == 1){//倒计时即将结束
+                                //停止会掉函数的执行
+                                clearInterval(no);
+                                //还原sms_code_tip
+                                this.sms_code_tip = '获取短信验证码';
+                                //重新生成图形验证码
+                                this.generate_image_code();
+                            }
+                            else{
+                                num -= 1;
+                                this.sms_code_tip = num + '秒';
+                            }
+                        },1000);
+                    }
+                    else {
+                        if (response.data.code == "4001") {
+                            //图形验证码有误
+                            this.error_image_code_message = response.data.errmsg;
+                            this.error_image_code = true;
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.log(error.response)
+                })
         },
         //提交事件
         on_submit(){
